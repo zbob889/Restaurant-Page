aba218f6df34b15752ffd564953ff10aba3ef81a
zbob889 <zroberts889@gmail.com>
2022-06-24 13:29:16 +0200

    Finished

--- src/contact.js
+++ src/contact.js
@@ -3,4 +3,18 @@ import addText from './addText.js';
 
 export default function loadContact(){
 
+    addElement('div', 'card', 'contactContent', 'content');
+
+    addElement('p', '', 'contactTitle', 'contactContent');
+    addText('contactTitle', "Contact Information");
+
+    addElement('div', '', 'contactDiv', 'contactContent')
+
+
+    addElement('p', 'contactText', 'location', 'contactDiv');
+    addText('location', "<span class='bold'>Location:</span>  Velperweg 48, Arnhem");
+
+    addElement('p', 'contactText', 'phone', 'contactDiv');
+    addText('phone', "<span class='bold'>Phone Number:</span>  55 55 55 55 55");
+
 };
